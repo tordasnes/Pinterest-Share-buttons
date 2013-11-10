ec1caf9099726b7b8d3cc52b07b44a77584455b7
Tord Asnes <tord.aasnes@gmail.com>
2013-11-10 17:02:02 +0100

    Initial Commit

---
+++ PinterestFeature/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PinterestFeature
+//
+//  Created by Tord Åsnes on 10/11/13.
+//  Copyright (c) 2013 Tord Åsnes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
